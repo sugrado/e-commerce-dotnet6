ec5f1dc59d1e9f2b7072c4841502d8f33cd2db13
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-05-22 13:11:06 +0300

    code cleanup

--- Core/ECommerceBackend.Application/Repositories/Products/IProductWriteRepository.cs
+++ Core/ECommerceBackend.Application/Repositories/Products/IProductWriteRepository.cs
@@ -1,10 +1,5 @@
 ﻿using ECommerceBackend.Application.CorePackages.Persistence;
 using ECommerceBackend.Domain.Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerceBackend.Application.Repositories.Products
 {
