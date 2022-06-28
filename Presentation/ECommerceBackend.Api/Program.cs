17b3e356199de3935c9e28389051982919f1a387
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-06-29 00:55:25 +0300

    add CORS policy

--- Presentation/ECommerceBackend.Api/Program.cs
+++ Presentation/ECommerceBackend.Api/Program.cs
@@ -3,6 +3,11 @@ using ECommerceBackend.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistenceServices();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+    policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()
+));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
