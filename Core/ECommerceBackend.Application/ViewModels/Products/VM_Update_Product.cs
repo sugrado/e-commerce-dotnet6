c3a448ebff64c720e34425b25d4e2967ef313035
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-14 11:24:03 +0300

    product CRUD operations for test

---
+++ Core/ECommerceBackend.Application/ViewModels/Products/VM_Update_Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceBackend.Application.ViewModels.Products
+{
+    public class VM_Update_Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Presentation/ECommerceBackend.Api/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using ECommerceBackend.Application.Repositories.Customers;
+using ECommerceBackend.Application.Repositories.Orders;
+using ECommerceBackend.Application.Repositories.Products;
+using ECommerceBackend.Application.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceBackend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly IOrderReadRepository _orderReadRepository;
+        private readonly ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository,
+            IProductReadRepository productReadRepository,
+            IOrderWriteRepository orderWriteRepository,
+            ICustomerWriteRepository customerWriteRepository,
+            IOrderReadRepository orderReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_productReadRepository.GetAll(false));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok(_productReadRepository.GetByIdAsync(id, false));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            var x = await _productWriteRepository.AddAsync(new() { Name = model.Name, Price = model.Price, Stock = model.Stock });
+            await _productWriteRepository.SaveAsync();
+            Thread.Sleep(5000);
+            return Ok(x);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model)
+        {
+            var product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Price = model.Price;
+            product.Stock = model.Stock;
+            product.Name = model.Name;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+    }
+}
