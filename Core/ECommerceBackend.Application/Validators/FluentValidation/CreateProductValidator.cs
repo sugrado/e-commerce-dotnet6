3bee2c58d1b980c393210b2f0f063d271be1e029
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-07-14 11:23:38 +0300

    installed FluentValidation and configured api behavior options

---
+++ Core/ECommerceBackend.Application/Validators/FluentValidation/CreateProductValidator.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceBackend.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ECommerceBackend.Application.Validators.FluentValidation
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Product name is required!")
+                .MaximumLength(150)
+                .MinimumLength(3)
+                    .WithMessage("The length of the product name must be between 5 and 150 characters.");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Stock info is required.")
+                .Must(s => s >= 0)
+                    .WithMessage("Stock info must be greater than zero.");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Price info is required.")
+                .Must(s => s >= 0)
+                    .WithMessage("Price info must be greater than zero.");
+        }
+    }
+}
--- Presentation/ECommerceBackend.Api/Program.cs
+++ Presentation/ECommerceBackend.Api/Program.cs
@@ -1,4 +1,8 @@
+using ECommerceBackend.Application.Validators.FluentValidation;
+using ECommerceBackend.Infrastructure.ActionHandlers.Filters;
 using ECommerceBackend.Persistence;
+using FluentValidation.AspNetCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,11 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()
 ));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddFluentValidation(config => config.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
